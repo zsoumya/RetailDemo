002b89e5037449866e0f4744fce0e051eeb37813
Soumya Mukherjee <zsoumya@yahoo.com>
2018-08-01 19:12:18 -0400

    Added Sales project

---
+++ Sales/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Sales {
+    using System;
+    using System.Threading.Tasks;
+
+    using NServiceBus;
+
+    internal class Program {
+        private static async Task Main() {
+            Console.Title = "Sales";
+
+            var endpointConfiguration = new EndpointConfiguration("Sales");
+            endpointConfiguration.UseTransport<LearningTransport>();
+
+            IEndpointInstance endpointInstance = await Endpoint.Start(endpointConfiguration).ConfigureAwait(false);
+
+            Console.WriteLine("Press Enter to exit.");
+            Console.ReadLine();
+
+            await endpointInstance.Stop().ConfigureAwait(false);
+        }
+    }
+}
